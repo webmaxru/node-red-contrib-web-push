b834d4f31c7e7f38350defb0b4f82932b63f0fa7
bartbutenaers <bart.butenaers@gmail.com>
2020-04-07 22:08:08 +0200

    Http endpoint

--- vapid-configuration.js
+++ vapid-configuration.js
@@ -8,3 +8,33 @@ module.exports = function (RED) {
   }
   RED.nodes.registerType('vapid-configuration', VapidConfigurationNode)
 }
+module.exports = function (RED) {
+  const webpush = require('web-push');
+    
+  function VapidConfigurationNode (config) {
+    RED.nodes.createNode(this, config)
+    this.subject = config.subject
+    this.publicKey = config.publicKey
+    this.privateKey = config.privateKey
+    this.gcmApiKey = config.gcmApiKey
+  }
+  RED.nodes.registerType('vapid-configuration', VapidConfigurationNode)
+  
+  // Make the key pair generation available to the config screen (in the flow editor)
+  RED.httpAdmin.get('/vapid_configuration/generate_key_pair', RED.auth.needsPermission('vapid-configuration.write'), async function(req, res){
+    try {
+      // Generate a VAPID keypair
+      const vapidKeys = webpush.generateVAPIDKeys();
+ 
+      // Return public key and private key to the config screen (since they need to be stored in the node's credentials)
+      res.json({
+        publicKey: vapidKeys.publicKey,
+        privateKey: vapidKeys.privateKey
+      })
+    }
+    catch (err) {
+      console.log("Error while generating VAPID keypair: " + err)
+      res.status(500).json({error: 'Error while generating VAPID keypair'})
+    }
+  });
+}
